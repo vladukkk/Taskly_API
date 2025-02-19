f1607016b03260397671eda1c988e1356c5d2026
VladOSSss228 <vladuslavantonyak94@gmail.com>
2025-02-19 12:50:51 +0200

    struct

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Taskly_API.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
