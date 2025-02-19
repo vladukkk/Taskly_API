864a05472bbddab942d9294936e5c988d5193b9b
VladOSSss228 <vladuslavantonyak94@gmail.com>
2025-02-19 16:03:42 +0200

    Add Repository

--- DataAccess/Contracts/IRepository.cs
+++ DataAccess/Contracts/IRepository.cs
@@ -1,10 +1,17 @@
 ﻿
+using System.Linq.Expressions;
+
 namespace DataAccess.Contracts
 {
     public interface IRepository<TEntity> where TEntity : class, IEntity
     {
-        Task<IEnumerable<TEntity>> Get();
-        Task<TEntity> GetById(Guid Id);
+        Task<IEnumerable<TEntity>?> Get(
+            Expression<Func<TEntity,bool>>? filter = null
+            , Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>>? orderBy = null
+            , params Expression<Func<TEntity, object>>[] includeProperties);
+
+        Task<TEntity?> GetById(Guid Id
+            , params Expression<Func<TEntity, object>>[] includeProperties);
 
         Task Add(TEntity entity);
         void Attach(TEntity entity);
@@ -12,6 +19,6 @@ namespace DataAccess.Contracts
         Task Delete(Guid Id);
         void Update(TEntity entity);
 
-        Task Save();
+        Task SaveAsync();
     }
 }
---
+++ DataAccess/Repository/Repository.cs
@@ -0,0 +1,85 @@
+﻿using DataAccess.Context;
+using DataAccess.Contracts;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace DataAccess.Repository
+{
+    internal class Repository<TEntity> : IDisposable, IRepository<TEntity> where TEntity : class, IEntity
+    {
+        private readonly TaskDbContext _context;
+        private readonly DbSet<TEntity> _dbSet;
+
+        public Repository(TaskDbContext dbContext)
+        {
+            _context = dbContext;
+            _dbSet = dbContext.Set<TEntity>();
+        }
+
+        public async Task<IEnumerable<TEntity>?> Get(
+            Expression<Func<TEntity, bool>>? filter = null
+            ,Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>>? orderBy = null
+            , params Expression<Func<TEntity, object>>[] includeProperties)
+        {
+            IQueryable<TEntity> query = _dbSet;
+
+            if (filter != null)
+                query = query.Where(filter);
+
+            if (orderBy != null)
+                query = orderBy(query);
+
+            foreach (var property in includeProperties)
+                query = query.Include(property);
+
+            return await query.ToListAsync();
+        }
+
+        public async Task<TEntity?> GetById(Guid Id, params Expression<Func<TEntity, object>>[] includeProperties)
+        {
+            IQueryable<TEntity> query = _dbSet;
+
+            foreach (var prop in includeProperties)
+                query = query = query.Include(prop);
+
+            return await query.FirstOrDefaultAsync(e => e.Id == Id);
+        }
+
+        public async Task Add(TEntity entity)
+        {
+            await _dbSet.AddAsync(entity);
+        }
+
+        public void Attach(TEntity entity)
+        {
+            _dbSet.Attach(entity);
+        }
+
+        public async Task Delete(Guid Id)
+        {
+            var entity = await _dbSet.FindAsync(Id);
+            if (entity != null)
+                _dbSet.Remove(entity);
+        }
+
+        public void Update(TEntity entity)
+        {
+            _dbSet.Update(entity);
+        }
+
+        public async Task SaveAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+
+        public async ValueTask DisposeAsync()
+        {
+            await _context.DisposeAsync();
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}
